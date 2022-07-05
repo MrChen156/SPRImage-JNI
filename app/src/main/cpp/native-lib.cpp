0cacf1f5958eee9953d842314f8da43c0361e296
President <cmqddemail@163.com>
2022-07-05 11:27:40 +0800

    写完JNI部分，准备传参

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,24 @@
 #include <jni.h>
 #include <string>
+#include <opencv2/opencv.hpp>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_sprimage2_MainActivity_stringFromJNI(
+extern "C" JNIEXPORT jdouble JNICALL
+Java_com_example_sprimage2_MainActivity_computeFromJni(
         JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+        jobject /* this */,
+        jlong cur_addr,
+        jlong ref_addr) {
+    using namespace cv;
+    Mat& cur = *(Mat*)cur_addr;
+    Mat& ref = *(Mat*)ref_addr;
+    cur.convertTo(cur, CV_16S);
+    ref.convertTo(ref, CV_16S);
+    Mat result(cur.rows, cur.cols, CV_16S);
+    cv::absdiff(cur, ref, result);
+    std::vector<Mat> channel;
+    Mat red_channel = channel.at(2);
+    Mat sum_channel = channel.at(2) + channel.at(1) + channel.at(0);
+    Mat output = red_channel / sum_channel;
+    Scalar output_values = sum(output);
+    return output_values[0];
 }
--- app/src/main/java/com/example/sprimage2/MainActivity.java
+++ app/src/main/java/com/example/sprimage2/MainActivity.java
@@ -1,11 +1,23 @@
 package com.example.sprimage2;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.sprimage2.databinding.ActivityMainBinding;
+
+import org.opencv.core.Mat;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private ActivityMainBinding binding;
+    private final String TOP_TAG = "SPR-Image_2";
+    private Uri referenceImageUri = null;
+    private Uri currentImageUri = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +43,60 @@ public class MainActivity extends AppCompatActivity {
         tv.setText(stringFromJNI());
     }
 
+    ActivityResultLauncher<Intent> selectReferenceImageUri = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                Intent data = result.getData();
+                referenceImageUri = Objects.requireNonNull(data).getData();
+                if (referenceImageUri != null) {
+                    ImageView refView = (ImageView) findViewById(R.id.ReferenceView);
+                    refView.setImageURI(referenceImageUri);
+                }
+            }
+    );
+
+    ActivityResultLauncher<String> selectCurrentImageUri = registerForActivityResult(new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri uri) {
+                    currentImageUri = uri;
+                    if (currentImageUri != null) {
+                        ImageView curView = (ImageView) findViewById(R.id.CurrentView);
+                        curView.setImageURI(currentImageUri);
+                    }
+                }
+            });
+
+    public void selectReference(View view) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        selectReferenceImageUri.launch(intent);
+    }
+
+    public void selectCurrent(View view) {
+        selectCurrentImageUri.launch("image/*");
+    }
+
+    public void doComputation(View view) {
+        if (currentImageUri != null && referenceImageUri != null){
+            startComputation();
+        } else if (currentImageUri == null) {
+            selectCurrent(view);
+        } else if (referenceImageUri == null) {
+            selectReference(view);
+        }
+    }
+
+    public void startComputation(){
+
+    }
+
+    private native double computeFromJni(long mat_Addr_cur, long mat_Addr_ref);
+
     /**
      * A native method that is implemented by the 'sprimage2' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+//    public native String stringFromJNI();
 }
